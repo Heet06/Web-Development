885396616ea1a736cb75c53fd4aba5869cc3dc06
Heet Radadiya <74094994+Heet06@users.noreply.github.com>
2022-09-16 18:58:45 +0530

    Some major learning

--- lecture3/hello/urls.py
+++ lecture3/hello/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<str:name>", views.greet, name="greet"),
     path("heet", views.heet, name="heet"),
     path("david", views.david, name="david")
 ]
--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -3,10 +3,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello!")
-
+    return render(request, "hello/index.html")
 def heet(request):
     return HttpResponse("Hello, Heet!")
 
 def david(request):
     return(HttpResponse("Hello, David!"))
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
